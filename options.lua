f4e425e299ba731b4e5b3a97db672c0c385a4803
Samuel Harvey-Collins <samuel.harvey-collins@conjure.co.uk>
2024-02-06 15:16:01 +0000

    Add copilot and fix tab options

--- options.lua
+++ options.lua
@@ -7,6 +7,9 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    tabstop = 4, -- number of space in a tab
+    expandtab = false, -- disable the use of space in tab
+    shiftwidth = 4, -- number of space inserted for indentation
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
